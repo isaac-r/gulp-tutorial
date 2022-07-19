f202438308e35792375ba2643b25d8785a416c3c
isaac.richardson <isaacr828@gmail.com>
2022-07-19 09:50:29 +0100

    Compiling sass to minified css

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,17 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+var uglifycss = require('gulp-uglifycss');
 
 gulp.task('sass', function() {
   return gulp.src('./sass/*.sass')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./css'));
 });
-
+ 
+gulp.task('css', function () {
+  return gulp.src('./css/*.css')
+    .pipe(uglifycss({      
+      "uglyComments": true
+    }))
+    .pipe(gulp.dest('./dist/'));
+});
