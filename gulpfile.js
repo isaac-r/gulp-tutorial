73be442f99cea5034be7399c6ffb4728f916e48f
isaac.richardson <isaacr828@gmail.com>
2022-07-19 09:37:58 +0100

    Adding sass task to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,9 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+gulp.task('sass', function() {
+  return gulp.src('./sass/*.sass')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
